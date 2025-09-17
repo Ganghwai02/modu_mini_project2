767a237ed25be9bd4c71f110a115f90eb8ebe44c
Ganghwai02 <dkdldb9305@gmail.com>
2025-09-17 12:48:44 +0900

    Create app.js

---
+++ modu_mini_project/frontend/src/app.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const userRoutes = require('./routes/userRoutes');
+const interviewRoutes = require('./routes/interviewRoutes');
+
+const app = express();
+const port = 3000;
+
+// 미들웨어 설정은 라우트보다 먼저 실행되어야 합니다.
+// CORS 미들웨어 대신 직접 헤더를 추가하는 방식
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(express.json());// frontend/src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
